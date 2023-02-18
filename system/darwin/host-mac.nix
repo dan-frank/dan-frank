415932bdb056b49cd572d35ed1a8cc2d28908c04
Daniel Lucas <dan.frank.lucas@gmail.com>
2023-02-18 22:08:04 +0000

    Removed code which didn't add apps to spotlight

--- system/darwin/host-mac.nix
+++ system/darwin/host-mac.nix
@@ -22,37 +22,6 @@ in {
   #   ''
   # );
 
-  system.build.applications = pkgs.buildEnv {
-    name = "system-applications";
-    paths = config.environment.systemPackages;
-    pathsToLink = "/Applications";
-  };
-
-  system.activationScripts.applications.text = ''
-    # Set up applications.
-    echo "setting up /Applications/Nix Apps..." >&2
-
-    ourLink () {
-      local link
-      link=$(readlink "$1")
-      [ -L "$1" ] && [ "''${link#*-}" = 'system-applications/Applications' ]
-    }
-
-    # Clean up for links created at the old location in HOME
-    if ourLink ~/Applications; then
-      rm ~/Applications
-    elif ourLink ~/Applications/'Nix Apps'; then
-      rm ~/Applications/'Nix Apps'
-    fi
-
-    if [ ! -e '/Applications/Nix Apps' ] \
-       || ourLink '/Applications/Nix Apps'; then
-      ln -sfn ${cfg.build.applications}/Applications '/Applications/Nix Apps'
-    else
-      echo "warning: /Applications/Nix Apps is not owned by nix-darwin, skipping App linking..." >&2
-    fi
-  '';
-
   services.spacebar = {
     enable = true;
     package = pkgs.spacebar;
