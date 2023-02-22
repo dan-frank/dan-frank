35b59e8c98ea8fd6320cdb42e12e41115cced283
Daniel Lucas <dan.frank.lucas@gmail.com>
2023-02-22 20:44:34 +0000

    Sorted brew masApps alphabetically

--- system/darwin/brew.nix
+++ system/darwin/brew.nix
@@ -18,19 +18,19 @@
       "visual-studio-code"
     ];
     masApps = {
+      "1Password for Safari" = 1569813296;
+      "AdGuard for Safari" = 1440147259;
       "Bears Countdown" = 1536711520;
       "Bears Gratitude" = 6443609622;
-      "1Password for Safari" = 1569813296;
-      "DuckDuckGo Privacy for Safari" = 1482920575;
       "Dark Reader for Safari" = 1438243180;
-      Vimari = 1480933944;
-      "SponsorBlock for YouTube" = 1573461917;
-      "AdGuard for Safari" = 1440147259;
-      PiPifier = 1160374471;
+      "DuckDuckGo Privacy for Safari" = 1482920575;
       "JSON Peep for Safari" = 1458969831;
-      "scite extension" = 1551820111;
-      Pages = 409201541;
       Numbers = 409203825;
+      Pages = 409201541;
+      PiPifier = 1160374471;
+      "scite extension" = 1551820111;
+      "SponsorBlock for YouTube" = 1573461917;
+      Vimari = 1480933944;
     };
   };
 }
