f23478788f1d50b468d6adbe11898cd3b47f4ed1
Daniel Lucas <dan.frank.lucas@gmail.com>
2023-03-29 12:26:49 +0100

    Enabled DRM video for Firefox

--- modules/home/programs/firefox/config/browser-features.nix
+++ modules/home/programs/firefox/config/browser-features.nix
@@ -94,7 +94,7 @@
   # if enabled, automatically downloads the Widevine Content Decryption Module
   # provided by Google Inc. Details
   # (https://support.mozilla.org/en-US/kb/enable-drm#w_opt-out-of-cdm-playback-uninstall-cdms-and-stop-all-cdm-downloads)
-  "media.eme.enabled" = false;
+  "media.eme.enabled" = true;
   # Disables the Widevine Content Decryption Module provided by Google Inc.
   # Used for the playback of DRM-controlled HTML5 content Details
   # (https://support.mozilla.org/en-US/kb/enable-drm#w_disable-the-google-widevine-cdm-without-uninstalling)
