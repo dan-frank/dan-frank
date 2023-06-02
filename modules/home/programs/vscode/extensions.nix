d43fbcf6813d588921871d98570ae4f32961d078
dan-frank <dan.frank.lucas@gmail.com>
2023-06-02 10:33:31 +0100

    Extract VSCode extensions to separate file

---
+++ modules/home/programs/vscode/extensions.nix
@@ -0,0 +1,30 @@
+{ pkgs }:
+with pkgs.vscode-extensions;
+[
+  bbenoist.nix
+  # bierner.emojisense
+  bierner.markdown-checkbox
+  bierner.markdown-emoji
+  bradlc.vscode-tailwindcss
+  eamodio.gitlens
+  gencer.html-slim-scss-css-class-completion
+  github.github-vscode-theme
+  gruntfuggly.todo-tree
+  matklad.rust-analyzer
+  mhutchie.git-graph
+  ms-vscode-remote.remote-ssh
+  naumovs.color-highlight
+  pkief.material-icon-theme
+  scala-lang.scala
+  scalameta.metals
+  shardulm94.trailing-spaces
+  usernamehw.errorlens
+  vscodevim.vim
+] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+  {
+    name = "vscode-pets";
+    publisher = "tonybaloney";
+    version = "1.2.2";
+    sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+  }
+]
