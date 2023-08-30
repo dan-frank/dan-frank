bf764e8182899a3a88c2e164cd9f45372920bad3
dan-frank <dan.frank.lucas@gmail.com>
2023-08-30 14:53:35 +0100

    Update types.string -> types.str

--- modules/users.nix
+++ modules/users.nix
@@ -5,19 +5,19 @@ in
 {
   options.users.primaryUser = {
     username = mkOption {
-      type = with types; nullOr string;
+      type = with types; nullOr str;
       default = null;
     };
     fullName = mkOption {
-      type = with types; nullOr string;
+      type = with types; nullOr str;
       default = null;
     };
     email = mkOption {
-      type = with types; nullOr string;
+      type = with types; nullOr str;
       default = null;
     };
     github = mkOption {
-      type = with types; nullOr string;
+      type = with types; nullOr str;
       default = null;
     };
   };
