88a707dba17441befb2406042295b6696d508394
Daniel Lucas <dan.frank.lucas@gmail.com>
2023-10-27 12:12:47 +0100

    Add hssmerge git alias

--- home/git.nix
+++ home/git.nix
@@ -23,5 +23,15 @@ in
       github.user = user-info.github;
       pull.rebase = "false";
     };
+    aliases = {
+      hssmerge = ''
+        "!f() { \
+          git checkout master && git reset --hard origin/master; \
+          count=$(git rev-list --count $1 ^master); \
+          if [ "$count" -gt "1" ];then ff="--no-ff";else ff="--ff-only"; fi; \
+          git merge $ff $1 && git push -f origin $1 && git push origin master && git push origin --delete $1; \
+        }; f"
+      '';
+    };
   };
 }
