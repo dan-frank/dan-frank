7a35af21da0ae8e970aa3bf29cc53d30eb8ea3ff
Daniel Lucas <dan.frank.lucas@gmail.com>
2023-11-08 14:27:55 +0000

    Increased harpoon popup window size

--- modules/home/programs/neovim/config/plugins/harpoon.lua
+++ modules/home/programs/neovim/config/plugins/harpoon.lua
@@ -8,3 +8,18 @@ vim.keymap.set("n", "<C-a>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-s>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-z>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-x>", function() ui.nav_file(4) end)
+
+require("harpoon").setup({
+    menu = {
+        width = math.max(
+          math.floor(vim.api.nvim_win_get_width(0) * 0.35),
+          80
+        ),
+        height = math.max(
+          math.floor(vim.api.nvim_win_get_height(0) * 0.35),
+          15
+        ),
+    }
+})
+
+require("telescope").load_extension("harpoon")
