f290ddfa13c0a6032d936cc4cc7b4c627a93d8af
Daniel Lucas <dan.frank.lucas@gmail.com>
2024-03-05 20:51:26 +0000

    Improve build script to print just error and pretty message

--- scripts/nb.nix
+++ scripts/nb.nix
@@ -1,18 +1,26 @@
 { pkgs }:
-pkgs.writeShellApplication {
+let 
+  figletFonts = pkgs.fetchFromGitHub {
+    owner = "xero";
+    repo = "figlet-fonts";
+    rev = "master";
+    sha256 = "HmO968OYH5yWwsDRp7HVfWvp48upUv+YwZxcMxBHNSw=";
+  };
+in pkgs.writeShellApplication {
   name = "nb";
-  runtimeInputs = with pkgs; [];
+  runtimeInputs = with pkgs; [ figlet lolcat ripgrep ];
   text = ''
     nb() {
       if [ $# -eq 1 ]; then
         local systemName="$1"
 
-        echo "Building system for: $systemName"
+        figlet -f "${figletFonts}/ANSI Shadow.flf" "Building system: $systemName" | lolcat
 
         nix build \
           --extra-experimental-features nix-command \
           --extra-experimental-features flakes \
-          .#darwinConfigurations."$systemName".system
+          .#darwinConfigurations."$systemName".system &> nix-build.log ||
+          (rg error nix-build.log && false)
 
         ./result/sw/bin/darwin-rebuild switch --flake .#"$systemName"
       else
