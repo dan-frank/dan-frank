1d4d7a878a934836f469d98f063b79ad19d136ec
Daniel Lucas <dan.frank.lucas@gmail.com>
2024-03-26 08:44:26 +0000

    Make telescope vertical + add commands shortcut + add desc to keymaps

--- modules/home/programs/neovim/plugins/telescope/init.lua
+++ modules/home/programs/neovim/plugins/telescope/init.lua
@@ -1,17 +1,31 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 -- Goto
-vim.keymap.set('n', '<leader>D', builtin.lsp_definitions, {}) -- definitions
-vim.keymap.set('n', '<leader>U', builtin.lsp_references, {})  -- usages
+vim.keymap.set("n", "<leader>D", builtin.lsp_definitions, { desc = "Find definition of hovered" })
+vim.keymap.set("n", "<leader>U", builtin.lsp_references, { desc = "Find usages of hovered" })
 
 -- Find
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
--- vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Fuzzy search file names" })
+-- vim.keymap.set("n", "<leader>ps", function()
 --   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 -- end)
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, { desc = "Fuzzy search codebase" })
+vim.keymap.set("n", "<leader>pb", builtin.buffers, { desc = "Show open buffers" })
 
--- ???
--- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
--- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- Util
+vim.keymap.set("n", "<C-h>", builtin.help_tags, { desc = "Show neovim commands" })
+
+require("telescope").setup({
+  defaults = {
+    layout_strategy = "vertical",
+    layout_config = {
+      horizontal = {
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      width = 0.75,
+      height = 0.80,
+    },
+    file_ignore_patterns = { "node_modules" },
+  },
+})
